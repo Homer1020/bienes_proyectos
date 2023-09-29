f2e24c1d07a3a25b30588d9b166595fb5b7788f7
Homer Moncayo <homermoncallo@gmail.com>
2023-09-29 09:53:07 -0400

    Las rutoas estaban siendo definidas con el metodo .use, esta mal, porque ese habarca todos los methodos de solicitud (GET, POST, PATH, DELETE, etc...)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const app = require('./config/app')
 const routes = require('./routes/index')
 
-app.use('/formulario', routes)
-app.use('/bienes', routes)
-app.use('/solicitudes', routes)
-app.use('/solicitudes/detalles', routes)
+app.get('/formulario', routes)
+app.get('/bienes', routes)
+app.get('/solicitudes', routes)
+app.get('/solicitudes/detalles', routes)
 
 app.listen(app.get('PORT'), () => {
   console.log('Server in port ' + app.get('PORT'))
