9b9d3580766be58ceafc491f47a42541d484d907
Homer Moncayo <homermoncallo@gmail.com>
2023-09-29 09:56:22 -0400

    Se eliminaron rutas del index, estas deben estar en la carpeta routes.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 const app = require('./config/app')
-const routes = require('./routes/index')
-
-app.use('/formulario', routes)
-app.use('/bienes', routes)
-app.use('/solicitudes', routes)
-app.use('/solicitudes/detalles', routes)
 
 app.listen(app.get('PORT'), () => {
   console.log('Server in port ' + app.get('PORT'))
